d97ea1c75afe34154689f79816804b7b742da216
Mathias Jørgensen <mathias.jorqensen@gmail.com>
2024-06-10 23:27:24 +0200

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    prefetch: true,
-    integrations: [tailwind(), icon()],
-    markdown: {
-        shikiConfig: {
-            theme: "github-dark-default",
-        },
-    },
+  prefetch: true,
+  integrations: [tailwind(), icon()],
+  site: "https://jorqensen.dev",
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark-default"
+    }
+  },
+  output: "static",
+  adapter: vercelStatic()
 });
