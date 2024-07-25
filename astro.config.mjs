498feec0dbc40e9b1d021b6d128b026b5d7c30e9
Mathias Jørgensen <mathias.jorqensen@gmail.com>
2024-07-25 02:35:04 +0200

    add mdx for future use

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from "@astrojs/vercel/static";
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    prefetch: true,
-    integrations: [tailwind(), icon()],
-    site: "https://jorqensen.dev",
-    markdown: {
-        shikiConfig: {
-            theme: "github-dark-default",
-        },
-    },
-    output: "static",
-    adapter: vercelStatic(),
+  prefetch: true,
+  integrations: [tailwind(), icon(), mdx()],
+  site: "https://jorqensen.dev",
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark-default"
+    }
+  },
+  output: "static",
+  adapter: vercelStatic()
 });
